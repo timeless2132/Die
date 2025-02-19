9d1d3ef4ab519d34d129164b3bc9b443ef569eed
Carter Duffy <carterduffy4@gmail.com>
2025-02-19 14:26:56 -0500

    finished it

--- Die/Program.cs
+++ Die/Program.cs
@@ -1,20 +1,50 @@
-﻿namespace Die
-{
-    public class Die
-    {
-        
-        private Random _generator;
-        private int _roll;
+﻿using System.Security.Cryptography.X509Certificates;
 
-        public Die()
-        {
-            _generator = new Random(); 
-            _roll = _generator.Next(1, 7);
-        }
-        
+namespace Die
+{
+    internal class Program
+    {
         static void Main(string[] args)
         {
-           
+            die die1, die2;
+            die1 = new die();
+            die2 = new die();
+
+            if (die1.Roll == die2.Roll)
+            {
+                Console.WriteLine("Doubles");
+            }
+            else { Console.WriteLine("Not Doubles"); }
+
+            if (die1.Roll + die2.Roll == 7)
+            {
+                Console.WriteLine("They sum of the two numbers is 7");
+            }
+            else if (die1.Roll == 1 && die2.Roll == 1)
+            {
+                Console.WriteLine("Snake eyes"); 
+            }
+            else if (die1.Roll - 1 == die2.Roll || die1.Roll + 1 == die2.Roll || die2.Roll - 1 == die1.Roll || die2.Roll + 1 == die2.Roll)
+            {
+                Console.WriteLine("The numbers are sequential");
+            }
+            else if ((die1.Roll + die2.Roll) % 2 == 0)
+            {
+                Console.WriteLine("The numbers are even");
+            }
+
+            Console.WriteLine(die1);
+            die1.DrawDie();
+            Console.WriteLine(die2);
+            die2.DrawDie();
+            
+            if (die1.Roll > die2.Roll)
+            {
+                Console.WriteLine("Die 1 is greater than die 2");
+            }
+            else
+                Console.WriteLine("Die 2 is greater than die 1");
         }
     }
+
 }
